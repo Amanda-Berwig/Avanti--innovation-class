61fa821d2e632f3498e7f880d298cb4dfa47ee8b
Amanda Berwig <amanda.berwig@gmail.com>
2025-04-04 18:47:30 -0300

    Ajusta o estilo e a responsividade do swiper e primeira seção (SALE) melhorando a exibição em diferentes tamanhos de tela

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ const swiper = new Swiper(".swiper", {
       spaceBetween: 10,
     },
     640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+      slidesPerView: 3,
+      spaceBetween: 15,
     },
     768: {
       slidesPerView: 3,
-      spaceBetween: 30,
+      spaceBetween: 15,
     },
     1024: {
       slidesPerView: 5,
-      spaceBetween: 20,
+      spaceBetween: 15,
     },
   },
   // If we need pagination
